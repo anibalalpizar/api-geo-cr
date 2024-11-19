a0ffc7d5b43ce140b9f9dc74c2ec3f1ecbabde1b
anibalalpizar <anibal.alpizar14@gmail.com>
2024-11-19 00:41:42 -0600

    add provinciasData for predefined provincia instances

---
+++ src/infrastructure/persistence/provincia.data.ts
@@ -0,0 +1,11 @@
+import { Provincia } from '@/domain/provincia.entity';
+
+export const provinciasData: Provincia[] = [
+  Provincia.create(1, 'San José'),
+  Provincia.create(2, 'Alajuela'),
+  Provincia.create(3, 'Heredia'),
+  Provincia.create(4, 'Cartago'),
+  Provincia.create(5, 'Guanacaste'),
+  Provincia.create(6, 'Puntarenas'),
+  Provincia.create(7, 'Limón'),
+];
