d30b229fef94c9d04b0b9d6102b4fa0cea880b41
anibalalpizar <anibal.alpizar14@gmail.com>
2024-11-21 14:52:23 -0600

    feat: integrate DistritoModule and update canton module to use GetDistritosByCantonUseCase

--- src/interfaces/canton.module.ts
+++ src/interfaces/canton.module.ts
@@ -1,21 +1,23 @@
 import { GetAllCantonesUseCase } from '@/application/get-all-cantones.usecase';
 import { GetCantonByIdUseCase } from '@/application/get-canton-by-id.usecase';
-import { GetCantonesByProvinciaUseCase } from '@/application/get-cantones-by-provincia.usecase';
+import { GetDistritosByCantonUseCase } from '@/application/get-distritos-by-canton.usecase';
 import { InMemoryCantonRepository } from '@/infrastructure/persistence/canton.in-memory.repository';
 import { Module } from '@nestjs/common';
 import { CantonController } from './canton.controller';
+import { DistritoModule } from './distrito.module';
 
 @Module({
+  imports: [DistritoModule],
   controllers: [CantonController],
   providers: [
     GetAllCantonesUseCase,
     GetCantonByIdUseCase,
-    GetCantonesByProvinciaUseCase,
+    GetDistritosByCantonUseCase,
     {
       provide: 'CantonRepository',
       useClass: InMemoryCantonRepository,
     },
   ],
-  exports: [GetCantonesByProvinciaUseCase],
+  exports: [GetDistritosByCantonUseCase, 'CantonRepository'],
 })
 export class CantonModule {}
