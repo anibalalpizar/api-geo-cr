58c81fb9e90e5b8d9a3bc38b5a90905853fabced
anibalalpizar <anibal.alpizar14@gmail.com>
2024-11-21 03:17:23 -0600

    feat: integrate Swagger for API documentation with initial configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Geo CR')
+    .setDescription('API para obtener información geográfica de Costa Rica')
+    .setVersion('1.0')
+    .addTag('provincias', 'Endpoints relacionados con provincias')
+    .addTag('cantones', 'Endpoints relacionados con cantones')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
