57c40b4ce89b74e51fa85c5f8bda75e923ed8fda
anibalalpizar <anibal.alpizar14@gmail.com>
2024-11-21 02:15:51 -0600

    feat: enhance ProvinciaModule with use cases and in-memory repository

--- src/interfaces/provincia.module.ts
+++ src/interfaces/provincia.module.ts
@@ -1,7 +1,21 @@
+import { GetAllProvinciasUseCase } from '@/application/get-all-provincias.usecase';
+import { GetProvinciaByIdUseCase } from '@/application/get-provincia-by-id.usecase';
+import { InMemoryProvinciaRepository } from '@/infrastructure/persistence/provincia.in-memory.repository';
 import { Module } from '@nestjs/common';
+import { CantonModule } from './canton.module';
 import { ProvinciaController } from './provincia.controller';
 
 @Module({
+  imports: [CantonModule],
   controllers: [ProvinciaController],
+  providers: [
+    GetAllProvinciasUseCase,
+    GetProvinciaByIdUseCase,
+    {
+      provide: 'ProvinciaRepository',
+      useClass: InMemoryProvinciaRepository,
+    },
+  ],
 })
 export class ProvinciaModule {}
+
