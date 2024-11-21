ef9712b24f1a8c9e75e7f457caa591da4d1008b6
anibalalpizar <anibal.alpizar14@gmail.com>
2024-11-21 14:51:01 -0600

    feat: add DistritoModule to app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppService } from '@/app.service';
 import { CantonModule } from '@/interfaces/canton.module';
 import { ProvinciaModule } from '@/interfaces/provincia.module';
 import { Module } from '@nestjs/common';
+import { DistritoModule } from './interfaces/distrito.module';
 
 @Module({
-  imports: [ProvinciaModule, CantonModule],
+  imports: [ProvinciaModule, CantonModule, DistritoModule],
   controllers: [AppController],
   providers: [AppService],
 })
