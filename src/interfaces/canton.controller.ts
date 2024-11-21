507830a700d1ff63d87dcd3a1fe61529b8079453
anibalalpizar <anibal.alpizar14@gmail.com>
2024-11-21 03:17:34 -0600

    feat: enhance CantonController with Swagger documentation for findAll method

--- src/interfaces/canton.controller.ts
+++ src/interfaces/canton.controller.ts
@@ -2,12 +2,33 @@ import { GetAllCantonesUseCase } from '@/application/get-all-cantones.usecase';
 import { ResponseMessages, ResponseStatus } from '@/utils/response-status.enum';
 import { generateApiResponse } from '@/utils/response.util';
 import { Controller, Get, Query } from '@nestjs/common';
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('cantones')
 @Controller('cantones')
 export class CantonController {
   constructor(private readonly getAllCantonesUseCase: GetAllCantonesUseCase) {}
 
   @Get()
+  @ApiOperation({ summary: 'Obtener todos los cantones' })
+  @ApiQuery({
+    name: 'page',
+    required: false,
+    type: Number,
+    description: 'Número de página',
+    example: 1,
+  })
+  @ApiQuery({
+    name: 'limit',
+    required: false,
+    type: Number,
+    description: 'Cantidad de items por página',
+    example: 7,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Lista de cantones obtenida exitosamente',
+  })
   async findAll(@Query('page') page = 1, @Query('limit') limit = 7) {
     const pagination = { page: Number(page), limit: Number(limit) };
     const result = await this.getAllCantonesUseCase.execute(pagination);
