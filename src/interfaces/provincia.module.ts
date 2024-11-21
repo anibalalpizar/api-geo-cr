0785159ca793a132dd18d7cfe9f15df832ab5fcf
anibalalpizar <anibal.alpizar14@gmail.com>
2024-11-21 14:51:55 -0600

    feat: add GetCantonesByProvinciaUseCase to ProvinciaModule providers

--- src/interfaces/provincia.module.ts
+++ src/interfaces/provincia.module.ts
@@ -1,4 +1,5 @@
 import { GetAllProvinciasUseCase } from '@/application/get-all-provincias.usecase';
+import { GetCantonesByProvinciaUseCase } from '@/application/get-cantones-by-provincia.usecase';
 import { GetProvinciaByIdUseCase } from '@/application/get-provincia-by-id.usecase';
 import { InMemoryProvinciaRepository } from '@/infrastructure/persistence/provincia.in-memory.repository';
 import { Module } from '@nestjs/common';
@@ -11,6 +12,7 @@ import { ProvinciaController } from './provincia.controller';
   providers: [
     GetAllProvinciasUseCase,
     GetProvinciaByIdUseCase,
+    GetCantonesByProvinciaUseCase,
     {
       provide: 'ProvinciaRepository',
       useClass: InMemoryProvinciaRepository,
