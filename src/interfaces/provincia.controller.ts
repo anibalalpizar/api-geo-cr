41eccc718180ede0b981358a1c070b5a01f05880
anibalalpizar <anibal.alpizar14@gmail.com>
2024-11-21 02:18:21 -0600

    feat: add endpoint to fetch cantones by provincia ID in ProvinciaController

--- src/interfaces/provincia.controller.ts
+++ src/interfaces/provincia.controller.ts
@@ -1,19 +1,17 @@
 import { GetAllProvinciasUseCase } from '@/application/get-all-provincias.usecase';
+import { GetCantonesByProvinciaUseCase } from '@/application/get-cantones-by-provincia.usecase';
 import { GetProvinciaByIdUseCase } from '@/application/get-provincia-by-id.usecase';
-import { InMemoryProvinciaRepository } from '@/infrastructure/persistence/provincia.in-memory.repository';
 import { ResponseMessages, ResponseStatus } from '@/utils/response-status.enum';
 import { generateApiResponse } from '@/utils/response.util';
 import { Controller, Get, Param, Query } from '@nestjs/common';
 
 @Controller('provincias')
 export class ProvinciaController {
-  private readonly provinciaRepository = new InMemoryProvinciaRepository();
-  private readonly getAllProvinciasUseCase = new GetAllProvinciasUseCase(
-    this.provinciaRepository,
-  );
-  private readonly getProvinciaByIdUseCase = new GetProvinciaByIdUseCase(
-    this.provinciaRepository,
-  );
+  constructor(
+    private readonly getAllProvinciasUseCase: GetAllProvinciasUseCase,
+    private readonly getProvinciaByIdUseCase: GetProvinciaByIdUseCase,
+    private readonly getCantonesByProvinciaUseCase: GetCantonesByProvinciaUseCase,
+  ) {}
 
   @Get()
   async findAll(@Query('page') page = 1, @Query('limit') limit = 7) {
@@ -33,5 +31,27 @@ export class ProvinciaController {
   @Get(':id')
   async findById(@Param('id') id: string) {
     return this.getProvinciaByIdUseCase.execute(Number(id));
+  }
+
+  @Get(':id/cantones')
+  async findCantonesByProvinciaId(
+    @Query('page') page = 1,
+    @Query('limit') limit = 7,
+    @Param('id') id: string,
+  ) {
+    const pagination = { page: Number(page), limit: Number(limit) };
+    const result = await this.getCantonesByProvinciaUseCase.execute(
+      pagination,
+      Number(id),
+    );
+
+    return generateApiResponse(
+      ResponseStatus.SUCCESS,
+      ResponseMessages.CANTONES_FETCHED_SUCCESSFULLY,
+      result.data,
+      pagination.page,
+      pagination.limit,
+      result.meta.totalItems,
+    );
   }
 }
