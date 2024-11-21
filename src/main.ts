732ffb700e071fcf346862d9a6ec88e39391d01c
anibalalpizar <anibal.alpizar14@gmail.com>
2024-11-21 17:42:23 -0600

    feat: enhance Swagger configuration and simplify app bootstrap process

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
     .setTitle('API Geo CR')
     .setDescription('API para obtener información geográfica de Costa Rica')
     .setVersion('1.0')
+    .addTag('provincias', 'Endpoints relacionados con provincias')
+    .addTag('cantones', 'Endpoints relacionados con cantones')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/', app, document);
 
-  app.useStaticAssets(
-    join(__dirname, '..', 'node_modules', '@nestjs', 'swagger', 'dist'),
-  );
-
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
