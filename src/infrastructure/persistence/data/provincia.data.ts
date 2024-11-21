3f9b08ac12cc980ac2610a627a43a11ca472a2e4
anibalalpizar <anibal.alpizar14@gmail.com>
2024-11-21 02:20:06 -0600

    feat: add provinciasData file to persistence layer with initial provincia entries

---
+++ src/infrastructure/persistence/data/provincia.data.ts
@@ -0,0 +1,11 @@
+import { Provincia } from '@/domain/entities';
+
+export const provinciasData: Provincia[] = [
+  Provincia.create(1, 'San José'),
+  Provincia.create(2, 'Alajuela'),
+  Provincia.create(3, 'Heredia'),
+  Provincia.create(4, 'Cartago'),
+  Provincia.create(5, 'Guanacaste'),
+  Provincia.create(6, 'Puntarenas'),
+  Provincia.create(7, 'Limón'),
+];
