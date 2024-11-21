d2a708736fbbaf2023ed0ec2849d2f0c86257d21
anibalalpizar <anibal.alpizar14@gmail.com>
2024-11-21 02:24:40 -0600

    feat: add dependency injection for ProvinciaRepository in GetProvinciaById use case

--- src/application/get-provincia-by-id.usecase.ts
+++ src/application/get-provincia-by-id.usecase.ts
@@ -1,8 +1,13 @@
+import { Injectable, Inject } from '@nestjs/common';
 import type { Provincia } from '@/domain/entities';
 import { ProvinciaRepository } from '@/domain/repositories';
 
+@Injectable()
 export class GetProvinciaByIdUseCase {
-  constructor(private readonly provinciaRepository: ProvinciaRepository) {}
+  constructor(
+    @Inject('ProvinciaRepository')
+    private readonly provinciaRepository: ProvinciaRepository,
+  ) {}
 
   async execute(idProvincia: number): Promise<Provincia | null> {
     return this.provinciaRepository.findById(idProvincia);
