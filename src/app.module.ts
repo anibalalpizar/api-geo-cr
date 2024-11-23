e2b9bb23fef20bc2630d0fea59d4de5ec3a546f5
anibalalpizar <anibal.alpizar14@gmail.com>
2024-11-23 00:32:00 -0600

    feat: add global configuration module for environment settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,20 @@ import { AppService } from '@/app.service';
 import { CantonModule } from '@/interfaces/canton.module';
 import { ProvinciaModule } from '@/interfaces/provincia.module';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import envConfig from './config/env.config';
 import { DistritoModule } from './interfaces/distrito.module';
 
 @Module({
-  imports: [ProvinciaModule, CantonModule, DistritoModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [envConfig],
+      isGlobal: true,
+    }),
+    ProvinciaModule,
+    CantonModule,
+    DistritoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
